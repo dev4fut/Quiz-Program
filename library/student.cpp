b7aa05d5b9566754581934097aad21794a449e0b
pikamanh <hungmanh190204.github@gmail.com>
2023-07-07 22:13:27 +0700

    trash

---
+++ library/student.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include "Student.h"
+
+using namespace std;
+
+
+void Student::InputStudent() {
+    
+    cout << "Input Student ID: ";
+    cin >> this->id;
+    cout << "\nStudent Name: ";
+    cin >> this->name;
+    cout << "\nStudent Number: ";
+    cin >> this->numberStudent;
+    cout << "\nStudent grade: ";
+    cin >> this->grade;
+}
+
+void Student::OutputStudent() {
+    cout << "ID: " << id;
+    cout << "Name: " << name;
+    cout << "Student Number: " << numberStudent;
+    cout << "Student grade: " << grade;
+}
+
+void Student::UpdateStudent() {
+    string temp;
+    cout << "ID: ";
+    cin >> temp;
+    if (temp.length() != 0) {
+        this->id;           
+    }
+    cout << "\nName: ";
+    cin >> temp;
+    if (temp.length() != 0) {
+        this->name;
+    }
+    cout << "\nStudent Number: ";
+    cin >> temp;
+    if (temp.length() != 0) {
+        this->numberStudent;
+    }
+    cout << "\nStudent grade: " << endl;
+    cin >> temp;
+    if (temp.length() != 0) {
+        this->grade;
+    }
+}
+
+void Student::DeleteStudent() {
+    
+}
